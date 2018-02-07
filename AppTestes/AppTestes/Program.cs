253ae069b0489c4b5184c01460b601f24a6a8d34
Eli Pedro dos Santos <elipedrodos.santos@gmail.com>
2018-02-07 17:03:01 -0200

    Take e Skip

---
+++ AppTestes/AppTestes/Program.cs
@@ -0,0 +1,26 @@
+﻿using AppTestes.SKIP_TAKE;
+using System;
+
+namespace AppTestes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute();
+            Console.ReadLine();
+        }
+
+        private static async void Execute()
+        {
+            var resultado = await Skip_Take.GetSkip();
+
+            var resultado2 = await Skip_Take.GetTake();
+
+            resultado.ForEach(c => Console.WriteLine(c));
+
+            resultado2.ForEach(c => Console.WriteLine(c));
+
+        }
+    }
+}
---
+++ AppTestes/AppTestes/SKIP_TAKE/Skip_Take.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppTestes.SKIP_TAKE
+{
+    public class Skip_Take
+    {
+       public static async Task<List<int>> GetSkip()
+       {
+            await Task.Delay(0);
+
+            int[] grades = { 59, 82, 70, 56, 92, 98, 85 };
+            var filtro = grades.Skip(3).ToList();
+         
+            return filtro;
+       }
+
+        public static async Task<List<int>> GetTake()
+        {
+            await Task.Delay(0);
+
+            int[] grades = { 59, 82, 70, 56, 92, 98, 85 };
+            var filtro = grades.Take(3).ToList();
+
+            return filtro;
+        }
+    }
+}
